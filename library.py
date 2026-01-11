9f9458c1dbf4becb0cd24225ea56bd0f375a3e7f
Apurva Raj <apurva.raj2023@vitstudent.ac.in>
2026-01-11 22:21:24 +0530

    Login branch update

--- library.py
+++ library.py
@@ -3,3 +3,4 @@ def register_student(name, reg_no):
     print(f"Student {name} with Reg No {reg_no} registered successfully")
 print("Login Module Loaded")
 print("User can login with username and password")
+print("Login branch update")
